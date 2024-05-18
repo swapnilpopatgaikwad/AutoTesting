fc4577839ac16f4ef7117d3d2ed986149b796261
SWAPNIL GAIKWAD <gswapnil367@gmail.com>
2024-05-18 10:47:12 +0530

    Add project files.

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using AutoTesting.ViewModel;
+
+namespace AutoTesting.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AutoTesting.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string userName = "";
+
+        [ObservableProperty]
+        private string displayUserName = "";
+
+        public MainViewModel()
+        {
+        }
+
+        [RelayCommand]
+        private void ClickButton()
+        {
+            DisplayUserName = UserName;
+        }
+    }
+}
